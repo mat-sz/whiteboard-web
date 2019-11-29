41db4ab097daa2bfb240ff5f428df5ec514d4d50
Mat Sz <contact@matsz.dev>
2019-11-29 21:12:32 +0100

    replace localhost with the current hostname

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Whiteboard from './Whiteboard';
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const loadingDiv = document.getElementById('loading');
 
-let whiteboard = new Whiteboard(canvas, 'ws://localhost:5000/ws', () => {
+let whiteboard = new Whiteboard(canvas, 'ws://' + location.hostname + ':5000/ws', () => {
     loadingDiv.style.display = 'none';
 });
 
